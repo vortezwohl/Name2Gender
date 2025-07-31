1fb2dd1a998a5d781a501733f7cbd162a65dec89
吴子豪 <vortez.wohl@gmail.com>
2025-07-31 14:48:26 +0800

    优化包结构

--- name2gender/__init__.py
+++ name2gender/__init__.py
@@ -2,3 +2,6 @@ import os
 
 __ROOT__ = os.path.dirname(os.path.abspath(__file__))
 __CACHE_DIR__ = os.path.join(__ROOT__, '.cache')
+
+from .model import download_model, SMALL_MODEL, BASE_MODEL
+from .__main__ import Gender, Name2Gender, load_model
