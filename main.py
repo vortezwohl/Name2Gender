bcd6085763a127087a89950ccb591209e71d98ab
吴子豪 <vortez.wohl@gmail.com>
2025-07-31 10:13:12 +0800

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from name2gender!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ name2gender/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+__ROOT__ = os.path.dirname(os.path.abspath(__file__))
+__CACHE_DIR__ = os.path.join(__ROOT__, '.cache')
