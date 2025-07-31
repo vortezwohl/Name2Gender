70b2548c8aa1678033c0b2add88849567bbb6bde
吴子豪 <vortez.wohl@gmail.com>
2025-07-31 20:32:33 +0800

    feat: 实现下载 卸载 推理脚本

--- name2gender/_entry_point.py
+++ name2gender/_entry_point.py
@@ -1,15 +1,49 @@
+import os
+
 import click
 
-from name2gender import Name2Gender, load_model
+from name2gender import Name2Gender, load_model, download_model
+from name2gender.model import __CACHE_DIR__
 
 
-@click.command()
-@click.argument('name', required=False)
+@click.group()
+@click.version_option(version='0.0.2.alpha')
+def main():
+    ...
+
+
+@main.command()
+@click.argument('name', required=True)
 @click.option('--threshold', '-t', default=.5, type=float, help='threshold for prediction')
 @click.option('--model', '-m', default='small', type=str, help='name2gender model')
-def main(name: str, threshold: float, model: str):
+def predict(name: str, threshold: float, model: str):
     n2g = Name2Gender(load_model(model_name=model))
     if not name:
         click.echo('Please provide at least one name.', err=True)
     gender, prob = n2g(name=name, return_probability=True, threshold=threshold)
-    click.echo(f'"{name}" is {gender.value} with an probability of {prob * 100:.2f}%.')
+    click.echo(f'"{name}" is {gender.value.upper()} with an probability of {prob * 100:.2f}%.')
+
+
+@main.command()
+@click.argument('model', default='name2gender-small', required=False)
+def install(model: str):
+    if not model:
+        click.echo('Please provide the model name.', err=True)
+    try:
+        download_model(model_name=model)
+        click.echo(f'Successfully installed model "{model}".')
+    except FileNotFoundError as e:
+        print(e)
+
+
+@main.command()
+@click.argument('model', required=False)
+def uninstall(model: str):
+    if not model:
+        click.echo('Please provide the model name.', err=True)
+    # try:
+    path = os.path.join(__CACHE_DIR__, model)
+    if os.path.exists(path):
+        os.remove(path)
+        click.echo(f'Model \"{model}\" is uninstalled.')
+    click.echo(f"Model \"{model}\" doesn't exists.")
