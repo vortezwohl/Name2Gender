a70b9be1f7ce6b17cd32fb8d849d1c90505644f7
吴子豪 <vortez.wohl@gmail.com>
2025-07-31 14:54:53 +0800

    update: 更新测试脚本

--- test/main.py
+++ test/main.py
@@ -2,4 +2,5 @@ from name2gender import Name2Gender
 
 if __name__ == '__main__':
     n2g = Name2Gender()
-    print(n2g('吴子豪', return_probability=True))
+    print(n2g('Otto', return_probability=True))
+    print(n2g('Jane', return_probability=True))
