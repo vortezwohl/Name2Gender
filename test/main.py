b07e0dbcae66f87df6717e179f7a50063e0bb80d
吴子豪 <vortez.wohl@gmail.com>
2025-08-07 19:39:00 +0800

    update: 更新测试脚本

--- test/main.py
+++ test/main.py
@@ -1,7 +1,7 @@
-from name2gender import Name2Gender
+from name2gender import Name2Gender, load_model
 
 if __name__ == '__main__':
-    n2g = Name2Gender()
+    n2g = Name2Gender(load_model('name2gender-small-en'))
     names = ['吴子豪', '吴彦祖', '梅艳芳', '张曼玉',
              'Donald Trump', 'Ivanka Trump',
              'Justin Bieber', 'The Weeknd',
@@ -9,4 +9,4 @@ if __name__ == '__main__':
              'Elizabeth II', 'Henry VIII']
     for name in names:
         _gender, _prob = n2g(name, return_probability=True)
-        print(name, 'is', _gender, 'with a probability of', f'{_prob * 100:.2f}%')
+        print(name, 'is <', _gender, '> with a probability of', f'{_prob * 100:.2f}%')
